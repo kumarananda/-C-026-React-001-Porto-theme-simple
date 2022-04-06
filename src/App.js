d8820f79fcff5a66c2480809bd84dd466c25b835
kumarananda <coderanandakumar@gmail.com>
2022-04-06 16:34:53 +0600

    upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+
+import Student from './Student';
+import Teacher from './Teacher';
+
+function App() {
+  return (
+    <>
+    <Student />
+    <Teacher />
+    <Student />
+    <Teacher />
+    <Student />
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/Student.js
@@ -0,0 +1,73 @@
+
+
+const Student = () => {
+    return (
+        <section className="student">
+                <div className="container py-5">
+                    <div className="row d-flex justify-content-center">
+                        <div className="col ">
+                            <div className="heading">
+                                <h2>Our Developers</h2>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nulla officiis saepe vero maxime blanditiis, dignissimos repellat assumenda! Rem sed reprehenderit magnam repellendus libero modi inventore rerum repellat similique sint.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row team-member ">
+
+                    <div className="col-md-3 my-3">
+                        <div className="card">
+                            
+                            <div className="card-body">
+                            <img className="card-img" src="https://avatars.githubusercontent.com/u/89941727?v=4" alt="" />
+                                <h2>Ananda Saha</h2>
+                                <h6>JavaScript Developer</h6>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-3 my-3">
+                        <div className="card">
+                           
+                            <div className="card-body">
+                            <img className="card-img" src="https://avatars.githubusercontent.com/u/95589865?s=400&u=4a344a85760890a9e3d27138312b06e9f50f29db&v=4" alt="" />
+                                <h2>Ananda Saha</h2>
+                                <h6>JavaScript Developer</h6>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-3 my-3">
+                        <div className="card">
+                            
+                            <div className="card-body">
+                            <img className="card-img" src="https://avatars.githubusercontent.com/u/78043170?v=4" alt="" />
+                                <h2>Ananda Saha</h2>
+                                <h6>JavaScript Developer</h6>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-3 my-3">
+                        <div className="card">
+                            <div className="card-body">
+                                <img className="card-img" src="https://lfwf.io/uploads/studentProfile/thumb/1625621718.png" alt="" />
+
+                                <h2>Ananda Saha</h2>
+                                <h6>JavaScript Developer</h6>
+
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+
+
+                </div>
+            
+        </section>
+
+
+
+    )
+}
+export default Student;
